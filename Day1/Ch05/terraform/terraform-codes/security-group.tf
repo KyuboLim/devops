d659cca734d263f3959d8dfcf220f8dfffde56ef
kyubo.lim <kyubo.lim@sk.com>
2022-09-19 15:07:55 +0900

    fix : terraform 리소스명 변경

--- Day1/Ch05/terraform/terraform-codes/security-group.tf
+++ Day1/Ch05/terraform/terraform-codes/security-group.tf
@@ -1,11 +1,11 @@
 
 resource "aws_security_group" "test-sg-eks-cluster" {
-  name        = "test-sg-eks-cluster"
-  description = "security_group for test-eks-cluster"
+  name        = "skt-user12-test-sg-eks-cluster"
+  description = "security_group for skt-user12-test-eks-cluster"
   vpc_id      = aws_vpc.test-vpc.id
 
   tags = {
-    Name = "test-sg-eks-cluster"
+    Name = "skt-user12-test-sg-eks-cluster"
   }
 }
 
@@ -13,20 +13,28 @@ resource "aws_security_group_rule" "test-sg-eks-cluster-ingress" {
 
   security_group_id = aws_security_group.test-sg-eks-cluster.id
   type              = "ingress"
-  description       = "ingress security_group_rule for test-eks-cluster"
+  description       = "ingress security_group_rule for skt-user12-test-eks-cluster"
   from_port         = 0
   to_port           = 0
   protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
+
+  tags = {
+    Name = "skt-user12-test-sg-eks-cluster-ingress"
+  }
 }
 
 resource "aws_security_group_rule" "test-sg-eks-cluster-egress" {
 
   security_group_id = aws_security_group.test-sg-eks-cluster.id
   type              = "egress"
-  description       = "egress security_group_rule for test-eks-cluster"
+  description       = "egress security_group_rule for skt-user12-test-eks-cluster"
   from_port         = 0
   to_port           = 0
   protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
+
+  tags = {
+    Name = "skt-user12-test-sg-eks-cluster-egress"
+  }
 }
